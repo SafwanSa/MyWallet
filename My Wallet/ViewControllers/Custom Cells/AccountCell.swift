ef1a6ff7edbbe4be7e65994ad244c93fb24a445f
Safwan <safwan9f@gmail.com>
2020-03-25 03:07:36 +0300

    --- 1- Add AccountCell, FinanceViewController, ProfileViewController, and HSUnderLineTextField 2- Remove StartBudgetViewController, decomposing the class into the previous two classes (FinanceViewController, ProfileViewController) 3- AddPaymentViewController, remove فواتير from the UIPicker data 4- Changing all the UI related to class mentioned

---
+++ My Wallet/ViewControllers/Custom Cells/AccountCell.swift
@@ -0,0 +1,47 @@
+//
+//  AccountCell.swift
+//  My Wallet
+//
+//  Created by Safwan Saigh on 25/03/2020.
+//  Copyright © 2020 Safwan Saigh. All rights reserved.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+    
+    @IBOutlet weak var txt_fname: UITextField!
+    @IBOutlet weak var txt_lname: UITextField!
+    @IBOutlet weak var txt_email: UITextField!
+
+    //MARK:- Intsance Vars
+    var dataSourceDelivery : DataSource?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        dataSourceDelivery = DataSource()
+        dataSourceDelivery?.dataSourceDelegate = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}
+extension AccountCell: DataSourceProtocol{
+    func getCosts(costs: [Float]) {}
+    func getMonths(months: [String]) {}
+    func paidDataUpdated(data: [[Payment]]) {}
+    func unpaidDataUpdated(data: [Payment]) {}
+    func userDataUpdated(data: [String : Any], which:String) {
+        if(which == "user"){
+            txt_email.text = data["Email"] as? String
+            txt_fname.text = data["First Name"] as? String
+            txt_lname.text = data["Last Name"] as? String
+        }
+    }
+    
+    
+}
---
+++ My Wallet/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ProTableViewController.swift
+//  My Wallet
+//
+//  Created by Safwan Saigh on 25/03/2020.
+//  Copyright © 2020 Safwan Saigh. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if(indexPath.section == 0){
+            return 112
+        }else{
+            return 44
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.section == 0){
+            let cell = Bundle.main.loadNibNamed("AccountCell", owner: self, options: nil)?.first as! AccountCell
+            return cell
+        }else if(indexPath.section == 1){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "financeInfo", for: indexPath)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "billMng", for: indexPath)
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(indexPath.section == 1){
+            self.performSegue(withIdentifier: "goToFinance", sender: self)
+        }else if(indexPath.section == 2){
+            self.performSegue(withIdentifier: "goToBillMng", sender: self)
+        }
+    }
+
+   
+  
+
+    
+
+}
