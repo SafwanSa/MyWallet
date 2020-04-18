41e30505ee9ee84c64960644cb409add7c9e5775
Safwan <safwan9f@gmail.com>
2020-04-18 14:38:21 +0300

    Add ForgotPasswordVC (Empty)

---
+++ My Wallet/ViewControllers/ForgotPasswordViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ForgotPasswordViewController.swift
+//  My Wallet
+//
+//  Created by Safwan Saigh on 17/04/2020.
+//  Copyright © 2020 Safwan Saigh. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+class ForgotPasswordViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+}
